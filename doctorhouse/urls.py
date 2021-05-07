4f6b1d9ff7c869a0b835d93477b7b85b30bbc158
Youssef Shaaban <y.shaaban@lit-co.net>
2021-05-07 19:20:27 +0300

    rename heartclinic app > heart_clinic app

--- doctorhouse/urls.py
+++ doctorhouse/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('heart_clinic/', include('heart_clinic.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ heart_clinic/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HeartClinicConfig(AppConfig):
+    name = 'heart_clinic'
