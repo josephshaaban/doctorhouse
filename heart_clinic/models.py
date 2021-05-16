b1a976a5950dd6c9c5325b1e30f12e3bb15e3ceb
Joseph Shaaban <y.shaaban@lit-co.net>
2021-05-16 21:33:04 +0300

    add HeartConsultationSystem

--- heart_clinic/models.py
+++ heart_clinic/models.py
@@ -41,14 +41,19 @@ class HeartConsultation(models.Model):
         verbose_name_plural = _('Heart consultations')
 
     full_name = models.CharField(verbose_name=_('Full name'), max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(verbose_name=_('Email'))
     consultation_title = models.CharField(verbose_name=_('Consultation title'), max_length=100)
-    phone_number = PhoneNumberField()
-    age = models.IntegerField(validators=[MaxValueValidator(150), MinValueValidator(1)])
-    gender = models.BooleanField(choices=(
-        (True, _('male')),
-        (False, _('female')),
-    ))
+    phone_number = PhoneNumberField(verbose_name=_('Phone number'))
+    age = models.IntegerField(
+        verbose_name=_('Age'),
+        validators=[MaxValueValidator(150), MinValueValidator(1)]
+    )
+    gender = models.BooleanField(
+        verbose_name=_('Gender'),
+        choices=(
+            (True, _('male')),
+            (False, _('female')),
+        ))
     medical_history = models.TextField(
         verbose_name=_('Medical history'),
         null=False, blank=False,
@@ -57,3 +62,60 @@ class HeartConsultation(models.Model):
         verbose_name=_('Consultation description'),
         null=False, blank=False,
     )
+
+
+class HeartConsultationSystem(models.Model):
+    class Meta:
+        verbose_name = _('Heart consultation system')
+        verbose_name_plural = _('Heart consultations system')
+
+    age = models.IntegerField(
+        verbose_name=_('Age'),
+        validators=[MaxValueValidator(150), MinValueValidator(1)],
+    )
+    chest_pain_type = models.CharField(
+        verbose_name=_('Chest pain type'),
+        max_length=32,
+        choices=(
+            ('asympt', _('Asymptote')),
+            ('atyp_angina', _('A typ angina')),
+            ('typ_angina', _('typ angina')),
+            ('non_anginal', _('Non anginal')),
+        ))
+    rest_blood_pressure = models.IntegerField(
+        verbose_name=_('Rest blood pressure'),
+        validators=[MaxValueValidator(250), MinValueValidator(30)],
+    )
+    blood_sugar = models.CharField(
+        verbose_name=_('Blood sugar'),
+        max_length=32,
+        choices=(
+            ('FALSE', _('False')),
+            ('True', _('True')),
+        ))
+    rest_electro = models.CharField(
+        verbose_name=_('Rest electro'),
+        max_length=32,
+        choices=(
+            ('normal', _('Normal')),
+            ('left_vent_hyper', _('Left vent hyper')),
+            ('st_t_wave_abnormality', _('st_t_wave_abnormality')),
+        ))
+    max_heart_rate = models.IntegerField(
+        verbose_name=_('Max heart rate'),
+        validators=[MaxValueValidator(250), MinValueValidator(30)],
+    )
+    exercice_angina = models.CharField(
+        verbose_name=_('Exercice angina'),
+        max_length=32,
+        choices=(
+            ('yes', _('Yes')),
+            ('no', _('No')),
+        ))
+    disease = models.CharField(
+        verbose_name=_('Disease'),
+        max_length=32,
+        choices=(
+            ('positive', _('Positive')),
+            ('negative', _('Negative')),
+        ))
